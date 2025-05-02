9e4a767fec36b62a18eb5f9e399cbc698e88bda4
endo <acac6343@gmail.com>
2025-05-02 15:01:26 +0900

    endo

--- lesson23/src/lesson23/Teacher2301.java
+++ lesson23/src/lesson23/Teacher2301.java
@@ -1,5 +1,13 @@
 package lesson23;
 
-public class Teacher2301 {
+public class Teacher2301 extends Worker2301 {
+	public Teacher2301(String name, int age) {
+		 super(Constant2301.TEACHER, name, age);
+	}
+	
+	public void doWork() {
+		 System.out.println("知識を教えます。");
+	}
+
 
 }
