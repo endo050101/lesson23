7118755bea809fe778f789e5a44869817556f41e
endo <acac6343@gmail.com>
2025-05-02 14:58:51 +0900

    endo

---
+++ lesson23/src/lesson23/Police2301.java
@@ -0,0 +1,12 @@
+package lesson23;
+
+public class Police2301 extends Worker2301 {
+	public Police2301(String name, int age) {
+		super(Constant2301, POLICE, name, age);
+	}
+	
+	public void doWork() {
+		System.out.println("地域や人々の安全を守ります");
+	}
+
+}
